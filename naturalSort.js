a56cd90d4a2491a8e1423b2cc5d2d50aef7bb364
kyle <xlorkhanx@gmail.com>
2014-01-02 09:54:32 -0800

    Fixing a reference error

--- naturalSort.js
+++ naturalSort.js
@@ -3,7 +3,7 @@
  * Author: Jim Palmer (based on chunking idea from Dave Koelle)
  */
 /*jshint unused:false */
-module.exports = function (a, b) {
+module.exports = function naturalSort (a, b) {
 	"use strict";
 	var re = /(^([+\-]?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?)?$|^0x[0-9a-f]+$|\d+)/gi,
 		sre = /(^[ ]*|[ ]*$)/g,
